1494759f2b74ee8742f8d25e154a34d6be11440f
Svetoslav47 <svetoslav2806@gmail.com>
2025-05-07 10:56:32 +0300

    andibu

--- Server/src/middleware/upload.middleware.ts
+++ Server/src/middleware/upload.middleware.ts
@@ -3,21 +3,6 @@ import path from 'path';
 import fs from 'fs';
 import { Request } from 'express';
 
-const uploadDir = path.join(__dirname, '../../uploads/barbers');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
-    cb(null, uploadDir);
-  },
-  filename: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) {
-    const ext = path.extname(file.originalname);
-    const name = file.fieldname + '-' + Date.now() + ext;
-    cb(null, name);
-  }
-});
 
 export const barberImageUpload = multer({
   storage: multer.memoryStorage(),
