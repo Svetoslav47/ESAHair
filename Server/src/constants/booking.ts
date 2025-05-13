941c73245068593e7cbe6aae5f6085fd1f4c8a3a
Svetoslav47 <svetoslav2806@gmail.com>
2025-05-13 19:38:23 +0300

    time interval

--- Server/src/constants/booking.ts
+++ Server/src/constants/booking.ts
@@ -1,6 +1,6 @@
 export const BOOKING_CONSTANTS = {
     DAYS_IN_ADVANCE: 90, // How many days in advance can appointments be booked
-    TIME_SLOT_INTERVAL: 5, // Minutes between each time slot
+    TIME_SLOT_INTERVAL: 30, // Minutes between each time slot
     DEFAULT_PROCEDURE_LENGTH: 60, // Default procedure length in minutes if not specified
     DEFAULT_START_HOUR: 9, // Default start hour (9 AM)
     DEFAULT_END_HOUR: 18 // Default end hour (6 PM)
